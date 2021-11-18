102e3b6ab8ca153a43c22c0019cc2e7d71021a6f
Gabriel Ramos Nascimento <gabrielramosnascimento195@gmail.com>
2021-11-18 18:35:52 -0400

    Update primo_thread_openmp.c

--- primo_thread_openmp.c
+++ primo_thread_openmp.c
@@ -8,36 +8,58 @@
 #define NUM_EXEC 10
 #define NUM_THREADS getNumThreads()
 
-long int N;
-int qtdElementos[QTD_MAX] = {10000, 20000, 50000, 100000, 500000, 1000000 };
+long int N, num_threads;
+long int qtdElementos[QTD_MAX] = {100000, 200000, 50000, 10000000, 50000000, 500000000 };
+int   qtdThreads[QTD_MAX] = {2, 4, 8, 16, 32, 64};
 
 pthread_mutex_t mutexsum;
 int inicio=0, fim=999, count=0;
-
+void imprimePrimosSemThreads(int tid);
 void imprimePrimos(int tid);
 void OpenMP();
 void Threads();
 void *imprimePrimosThreads(void *tid);
 
 int getNumThreads(){
-	return (N / 1000)+1;
+	return num_threads;
 }
 
 int main(){
 	int i, j;
 	double tempo, soma=0,aux;
 	clock_t begin, end;
+	
 	for (j=0;j<QTD_MAX; j++){
 		N = qtdElementos[j];
-			
-		printf ("Executando N = %d\n", N);
+		num_threads = qtdThreads[j];
+		soma=0;	
+		printf ("\n-----------------------------------------------------\n");
+		printf ("Executando N = %d na Thread principal\n", N, getNumThreads());
+	
+		for (i=0;i<NUM_EXEC; i++){
+			//inicializando as variáveis
+			inicio=0;  count=0;
+			begin = clock();
+			imprimePrimosSemThreads(1);
+			end = clock();
+			tempo = (double)(end-begin)/CLOCKS_PER_SEC;
+			soma+=tempo;
+			printf ("Execucao %2d com %d numeros em %lf s\n",i+1,N,tempo);
+		}
+		printf ("Media de 10 execucoes com 1 thread em %lf s\n\n\n",soma/NUM_EXEC);
+		
+		printf ("Executando N = %d com %d Threads\n", N, getNumThreads());
 		for (i=0;i<NUM_EXEC;i++){
-			clock_t begin = clock();
+			//inicializando as variáveis
+			inicio=0; fim=999; count=0;
+			
+			//inicializando contador de tempo
+			begin = clock();
 			OpenMP();
-			clock_t end = clock();
+			end = clock();
 			tempo = (double)(end-begin)/CLOCKS_PER_SEC;
 			soma=soma+tempo;
-			aux=soma;
+
 			printf ("Execucao %2d openmp em %lf s\n",i+1,tempo);
 		}
 		
@@ -46,9 +68,13 @@ int main(){
 		soma=0;
 	
 		for (i=0;i<NUM_EXEC;i++){
-			clock_t begin = clock();
+			//inicializando as variáveis
+			inicio=0; fim=999; count=0;
+			
+			//inicializando contador de tempo
+			begin = clock();
 			Threads();
-			clock_t end = clock();
+			end = clock();
 			tempo = (double)(end-begin)/CLOCKS_PER_SEC;
 			soma+=tempo;
 			printf ("Execucao %2d thread em %lf s\n",i+1,tempo);
@@ -77,7 +103,7 @@ void imprimePrimos(int tid){
 		}
 		
 		if (cont){
-		// 	printf ("%5d\t",i);
+		 //	printf ("%5d\t",i);
 			k++; 
 			count++;
 			if (k==10){
@@ -88,6 +114,30 @@ void imprimePrimos(int tid){
 	}
 
 }
+
+
+void imprimePrimosSemThreads(int tid){
+	int cont=0,i, k,j;
+	int id = tid;
+	
+	k=0;
+	for (i=inicio;i<fim;i++){
+		if (i>1){
+			cont=1;
+			for (j=2;cont && j<=sqrt(i);j++)
+				if (i % j == 0){
+					cont = 0;
+					break;
+				}	
+		}
+		
+		if (cont){
+			//	printf ("%5d\t",i);
+		}
+	}
+
+}
+
 
 void OpenMP(){	
 	int thread_id, i;
